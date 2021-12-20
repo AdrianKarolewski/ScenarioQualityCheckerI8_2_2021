c4aabbac8f84b4b60957e993efbfe05412cc2eb7
julialamperska <julia.lamperska@gmail.com>
2021-12-20 23:17:22 +0100

    StepsCount class implementation

--- src/main/java/pl/put/poznan/sqc/logic/StepsCount.java
+++ src/main/java/pl/put/poznan/sqc/logic/StepsCount.java
@@ -5,8 +5,52 @@ import pl.put.poznan.sqc.scenario.Step;
 
 import java.util.ArrayList;
 
+/**
+ * StepsCount class implementation
+ */
 public class StepsCount implements ScenarioAbstractClass {
+
+    private int numberofsteps = 0;
+
+    /**
+     * StepsCount class constructor
+     *
+     * @param scenario in Scenario type
+     */
+    public StepsCount(Scenario scenario){
+        calculate(scenario);
+    }
+
+    /**
+     * A method checking and counting substeps of each scenario step (and substep - recursively)
+     *
+     * @param step in Step type
+     */
+    private void checkSubsteps(Step step){
+        if (step.getSubSteps().size() != 0) {
+            numberofsteps += step.getSubSteps().size();
+            for (Step subs : step.getSubSteps()){
+                checkSubsteps(subs);
+            }
+        }
+    }
+
+    /**
+     * A method calculating number of steps in a scenario
+     *
+     * @param scenario in Scenario type
+     */
     @Override
     public void calculate(Scenario scenario){
+
+        if (scenario.getSteps().size() == 0){
+            numberofsteps = 0;
+        }
+        else {
+            numberofsteps += scenario.getSteps().size();
+            for (Step s : scenario.getSteps()){
+                checkSubsteps(s);
+            }
+        }
     }
 }
